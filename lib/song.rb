c538524205ad411a14b6295fc03baf44e9befa19
Mitzi Estrada <m_estrada17@live.com>
2020-03-03 22:53:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,29 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song =self.create
+    song.name = song_name
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    self.all.detect{|song| song.name == song_name}
+  end
+  
+  
 
 end
